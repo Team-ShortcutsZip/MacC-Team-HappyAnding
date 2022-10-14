982fa7ab4b10aa0f890c6f4e5d9bb6dee4f8ad88
JIWON1923 <zest1923@gmail.com>
2022-10-14 17:09:26 +0900

    Project setting

---
+++ HappyAnding/HappyAnding/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HappyAnding
+//
+//  Created by 이지원 on 2022/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HappyAnding/HappyAnding/HappyAndingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyAndingApp.swift
+//  HappyAnding
+//
+//  Created by 이지원 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct HappyAndingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
