db93bf1340d21056e698190429b124cae39b83a1
Jeon Jimin <41153398+JMM00@users.noreply.github.com>
2022-10-21 19:50:24 +0900

    [Style] Add comment

--- HappyAnding/HappyAnding/Extensions/Color+Extension.swift
+++ HappyAnding/HappyAnding/Extensions/Color+Extension.swift
@@ -30,6 +30,8 @@ extension Color {
     static let Gray4 = Color("Gray4")
     static let Gray5 = Color("Gray5")
     
+    //gradient사용하는 곳에서 Color.fetchGradient(color: "Red")와 같이 사용해주세요
+    //컬러명은 숫자를 제외하고 UpperCamelCase로 입력해주세요
     static func fetchGradient(color: String) -> LinearGradient {
         let colors = [Color("\(color)01"), Color("\(color)02")]
         return LinearGradient(colors: colors, startPoint: .topLeading, endPoint: .bottomTrailing)
