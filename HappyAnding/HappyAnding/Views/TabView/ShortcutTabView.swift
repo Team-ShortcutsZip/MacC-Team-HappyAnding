b8beb0289ac69df81143b07d87391222912c64b0
otoolz <otool.hf@gmail.com>
2022-10-21 16:43:59 +0900

    [Fix] #7 탭바 error로 인한 rename
    TabView 동일 이름 사용으로 인한 Extra trailing closure passed in call 에러 발생
    -> ShortcutTabView로 이름 변경

---
+++ HappyAnding/HappyAnding/Views/TabView/ShortcutTabView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  HappyAnding
+//
+//  Created by 이지원 on 2022/10/14.
+//
+
+import SwiftUI
+
+struct ShortcutTabView: View {
+    var body: some View {
+        TabView {
+            ForEach(Tab.allCases, id: \.self) { tab in
+                tab.view
+                    .tabItem {
+                        Label(tab.tabName, image: tab.systemImage)
+                    }
+            }
+        }
+    }
+}
+
+struct ShortcutTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShortcutTabView()
+    }
+}
