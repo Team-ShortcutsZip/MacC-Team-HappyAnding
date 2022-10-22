9bcbf93b7f85100ddb052c172b20bb756971fa33
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-10-22 23:46:15 +0900

    [Feat] #37 divide Admin/User Curation view file

---
+++ HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadUserCurationView.swift
@@ -0,0 +1,20 @@
+//
+//  ReadUserCurationView.swift
+//  HappyAnding
+//
+//  Created by HanGyeongjun on 2022/10/22.
+//
+
+import SwiftUI
+
+struct ReadUserCurationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReadUserCurationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadUserCurationView()
+    }
+}
