953f4fb45241196b4156424741f0b46a7012cb40
otoolz <otool.hf@gmail.com>
2022-10-22 22:47:00 +0900

    [Feat] #5 ExploreShortcutView 다운로드 순위 추가
    ExploreShortcutView 다운로드 순위 추가

--- HappyAnding/HappyAnding/Views/ExploreShortcutViews/DownloadRankView.swift
+++ HappyAnding/HappyAnding/Views/ExploreShortcutViews/DownloadRankView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct DownloadRankView: View {
+    let shortcuts = Shortcut.fetchData(number: 5).sorted {
+        $0.numberOfDownload < $1.numberOfDownload
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("다운로드 순위")
+                    .Title2()
+                    .foregroundColor(Color.Gray5)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                Text("더보기")
+                    .Footnote()
+                    .foregroundColor(Color.Gray4)
+                    .padding(.trailing, 16)
+            }
+            .padding(.leading, 16)
+            
+            ForEach(Array(shortcuts.enumerated()), id:\.offset) { index, shortcut in
+                if index < 3 {
+                    ShortcutCell(color: shortcut.color, sfSymbol: shortcut.sfSymbol, name: shortcut.name, description: shortcut.description, numberOfDownload: shortcut.numberOfDownload, downloadLink: shortcut.downloadLink)
+                }
+            }
+        }
     }
 }
 
