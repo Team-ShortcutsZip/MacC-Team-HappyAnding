ba2b3425c4d9125b4ef76a56d741d8e9f643d7c7
otoolz <otool.hf@gmail.com>
2022-10-22 17:31:55 +0900

    [Feat] AddShortcutCardView UI
    AddShortcutCardView UI

--- HappyAnding/HappyAnding/Views/ExploreShortcutViews/MyShortcutCardView.swift
+++ HappyAnding/HappyAnding/Views/ExploreShortcutViews/MyShortcutCardView.swift
@@ -34,12 +34,28 @@ struct MyShortcutCardView: View {
         .padding()
         .frame(width: 107, height: 144, alignment: .leading)
         .background(Color.fetchGradient(color: mySHortcutColor))
-        .cornerRadius(15)
+        .cornerRadius(12)
+    }
+}
+
+struct AddMyShortcutCardView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "plus")
+                .font(.title2)
+                .foregroundColor(Color.Gray4)
+        }
+        .padding()
+        .frame(width: 107, height: 144)
+        .background(Color.Gray1)
+        .cornerRadius(12)
     }
 }
 
 struct MyShortcutCardView_Previews: PreviewProvider {
     static var previews: some View {
-        MyShortcutCardView(myShortcutIcon: "book.fill", myShortcutName: "노는게 제일조아", mySHortcutColor: "Coral")
+    //    MyShortcutCardView(myShortcutIcon: "book.fill", myShortcutName: "노는게 제일조아", mySHortcutColor: "Coral")
+        AddMyShortcutCardView()
     }
 }
