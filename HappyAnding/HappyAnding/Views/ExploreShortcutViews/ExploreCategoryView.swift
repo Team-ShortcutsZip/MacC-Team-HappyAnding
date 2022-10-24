8cea89f71152fd6c0dd3aa6a9b2631458499c9f8
Jeon Jimin <41153398+JMM00@users.noreply.github.com>
2022-10-25 02:40:02 +0900

    [Feat] #17 - apply category type

--- HappyAnding/HappyAnding/Views/ExploreShortcutViews/ExploreCategoryView.swift
+++ HappyAnding/HappyAnding/Views/ExploreShortcutViews/ExploreCategoryView.swift
@@ -25,26 +25,12 @@ struct ExploreCategoryView: View {
     let lovedShortcuts = Shortcut.fetchData(number: 10)
     let rankingShortcuts = Shortcut.fetchData(number: 10)
     
-    let category: String
-    let categoryDescriptions = [
-        "교육":"교육 카테고리에 대한 설명입니다.",
-        "금융":"금융 카테고리에 대한 설명입니다.",
-        "비즈니스":"비즈니스 카테고리에 대한 설명입니다.",
-        "건강 및 피트니스":"건강 및 피트니스 카테고리에 대한 설명입니다.",
-        "라이프스타일":"라이프스타일 카테고리에 대한 설명입니다.",
-        "날씨":"날씨 카테고리에 대한 설명입니다.",
-        "사진 및 비디오":"사진 및 비디오 카테고리에 대한 설명입니다.",
-        "꾸미기":"꾸미기 카테고리에 대한 설명입니다.",
-        "유틸리티":"유틸리티 카테고리에 대한 설명입니다.",
-        "소셜 네트워킹":"소셜 네트워킹 카테고리에 대한 설명입니다.",
-        "엔터테인먼트":"엔터테인먼트 카테고리에 대한 설명입니다.",
-        "여행":"여행 카테고리에 대한 설명입니다."
-    ]
+    let category: Category
     
     var body: some View {
         VStack {
             List {
-                Text(categoryDescriptions[category] ?? "")
+                Text(category.fetchDescription())
                     .foregroundColor(.Gray5)
                     .Body2()
                     .padding(16)
@@ -65,7 +51,7 @@ struct ExploreCategoryView: View {
                     .listRowSeparator(.hidden)
                     .listRowBackground(Color.Background)
                 Section() {
-                    ListHeader(title: "다운로드 순위")
+                    ListHeader(title: SectionType.download.rawValue)
                         .padding(.top, 20)
                         .listRowBackground(Color.Background)
                     ForEach(rankingShortcuts.indices, id: \.self) { index in
@@ -84,7 +70,7 @@ struct ExploreCategoryView: View {
                     .listRowSeparator(.hidden)
                 }
                 Section() {
-                    ListHeader(title: "사랑받는 단축어")
+                    ListHeader(title: SectionType.popular.rawValue)
                         .padding(.top, 20)
                         .listRowBackground(Color.Background)
                     ForEach(lovedShortcuts.indices, id: \.self) { index in
@@ -131,6 +117,6 @@ struct ListHeader: View {
 
 struct ExploreCategoryView_Previews: PreviewProvider {
     static var previews: some View {
-        ExploreCategoryView(category: "교육")
+        ExploreCategoryView(category: Category.business)
     }
 }
