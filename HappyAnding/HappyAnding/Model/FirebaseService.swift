20f485bf674220b3743b4fde01056903b706cc63
JIWON1923 <zest1923@gmail.com>
2022-10-25 21:16:47 +0900

    [Feat] #66 - delete function
    Co-authored-by: JMM00 <cjjunn0827@gmail.com>

--- HappyAnding/HappyAnding/Model/FirebaseService.swift
+++ HappyAnding/HappyAnding/Model/FirebaseService.swift
@@ -14,9 +14,9 @@ class FirebaseService {
     private let db = Firestore.firestore()
     
     func fetchShortcut() {
-        db.collection("Shortcut").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
+        db.collection("Shortcut").getDocuments() { (querySnapshot, error) in
+            if let error {
+                print("Error getting documents: \(error)")
             } else {
                 for document in querySnapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
@@ -26,11 +26,21 @@ class FirebaseService {
     }
     
     func createShortcut(shortcut: Shortcuts) {
-        db.collection("Shortcut").document(shortcut.id).setData(shortcut.dictionary) { err in
-            if let err = err {
-                print("Error writing document: \(err)")
+        db.collection("Shortcut").document(shortcut.id).setData(shortcut.dictionary) { error in
+            if let error {
+                print("Error writing document: \(error)")
             } else {
                 print("Document successfully written!")
+            }
+        }
+    }
+    
+    func deleteShortcut(shortcut: Shortcuts) {
+        db.collection("Shortcut").document(shortcut.id).delete() { error in
+            if let error {
+                print("Error removing document: \(error)")
+            } else {
+                print("Document successfully removed")
             }
         }
     }
