d1571326a6fa98ffee7c56a575109ca7d80e70f5
otoolz <otool.hf@gmail.com>
2022-10-25 12:44:12 +0900

    [Feat] #9 ReadShortcutView 헤더 뷰 UI
    ReadShortcutView 헤더 뷰 UI

--- HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutHeaderView.swift
+++ HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutHeaderView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct ReadShortcutHeaderView: View {
     var body: some View {
-        Text("ReadShortcutHeaderView")
+        VStack(alignment: .leading) {
+            HStack {
+                VStack {
+                    Image(systemName: "leaf")
+                        .foregroundColor(Color.White)
+                }
+                .frame(width: 52, height: 52)
+                .background(Color.fetchGradient(color: "Coral"))
+                .cornerRadius(8)
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "hand.thumbsup")
+                    Text("132")
+                }
+                .Body2()
+                .padding(10)
+                .foregroundColor(Color.Gray4)
+                .background(Color.Gray1)
+                .cornerRadius(12)
+            }
+            Text("Title")
+                .Title1()
+                .foregroundColor(Color.Gray5)
+            Text("simple descriasdfsafsafsafption")
+                .Body1()
+                .foregroundColor(Color.Gray3)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 16)
     }
 }
 
