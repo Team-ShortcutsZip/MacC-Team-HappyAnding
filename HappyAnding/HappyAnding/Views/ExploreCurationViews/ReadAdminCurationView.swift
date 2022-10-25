710b248a4bee8a0767534df5b9431030a0d12632
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-10-25 20:01:16 +0900

    [feat] #37 create sticky header

--- HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadAdminCurationView.swift
+++ HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadAdminCurationView.swift
@@ -15,45 +15,61 @@ import SwiftUI
  - subtitle: 큐레이션 부제목
  
  #description
+ - ScrollView Bounce가 아래쪽에서만 되게 해야 합니다.
  - 어드민 큐레이션 데이터가 만들어진 후 데이터 연결이 필요합니다.
  - 네비게이션 뷰 연결 및 설정이 필요합니다.
- - 공유버튼 액션 연결이 필요합니다.
+ - 공유버튼 액션 연결이 필요합니다. (스프린트2 이후)
  */
 
-struct ReadCurationView: View {
+struct ReadAdminCurationView: View {
     
-    //TODO: 큐레이션 데이터 모델 제작 후 해당 ObsrvdObjt 삭제 필요.
+    //TODO: 큐레이션 데이터 모델 제작 후 해당 ObservedObject 삭제 필요.
     @ObservedObject var shortcutData = fetchData()
-    
+        
     let title: String = "워라벨 지키기, 단축어와 함께"
     let subtitle: String = "워라벨을 알차게 지키고 있는 에디터도 애용하고 있는 단축어 모음."
     let curationThumbnail: String = "adminCurationTestImage"
     
     var body: some View {
-        NavigationView {
+//        NavigationView {
             ScrollView {
-                curationThumbnailImage
-                    .padding(.bottom, 20)
-                titleAndSubtitle
-                    .padding(.bottom, 20)
-                curationShortcutsListTest
-            }
-            .ignoresSafeArea()
-            .background(Color.Background)
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        //TODO: 네비게이션 연결 후 코드 지우기.
-                        //TODO: Share 버튼 눌렀을 때 Curation공유 링크 생성 및 actionSheet 나타내기
-                    }) {
-                        Image(systemName: "square.and.arrow.up")
-                            .foregroundColor(.Gray4)
-                    }
+                
+                GeometryReader { geo in
+                    let yOffset = geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0
+                    
+                    Image(curationThumbnail)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geo.size.width, height: geo.size.height - yOffset)
+                        .offset(y: yOffset)
                 }
+                .frame(height: 304)
+                
+//                curationThumbnailImage
+//                    .padding(.bottom, 20)
+                titleAndSubtitle
+                //각각의 단축어 셀 상단에 padding이 12씩 들어가 있음.
+                    .padding(.bottom, 8)
+                curationShortcutsListTest
+                Spacer()
+                    .frame(height: 44)
             }
+            .edgesIgnoringSafeArea(.top)
+            .background(Color.Background)
             
-        }
+            //TODO: 추후 공유 기능 추가 시 사용할 코드
+            //            .toolbar {
+            //                ToolbarItem(placement: .navigationBarTrailing) {
+            //                    Button(action: {
+            //                        //TODO: 네비게이션 연결 후 코드 지우기.
+            //                        //TODO: Share 버튼 눌렀을 때 Curation공유 링크 생성 및 actionSheet 나타내기
+            //                    }) {
+            //                        Image(systemName: "square.and.arrow.up")
+            //                            .foregroundColor(.Gray4)
+            //                    }
+            //                }
+            //            }
+//        }
     }
     
     ///최상단의 썸네일 이미지 영역입니다.
@@ -80,8 +96,7 @@ struct ReadCurationView: View {
     }
     
     ///큐레이션된 단축어 리스트입니다.
-    ///현재 테스트용으로 ListShortcutView의 코드를 재활용했습니다.
-    ///지금 제목과 큐레이션 사이 padding이 기존 20보다 더 들어가 있어 수정이 필요합니다.
+    ///현재 랜덤한 7개의 단축어를 가져와서 뷰를 그리도록 되어있습니다.
     //TODO: 큐레이션 데이터 모델 제작 후 적용 필요.
     var curationShortcutsListTest: some View {
         ForEach(0..<7, id: \.self) { index in
@@ -97,6 +112,6 @@ struct ReadCurationView: View {
 
 struct ReadCurationView_Previews: PreviewProvider {
     static var previews: some View {
-        ReadCurationView()
+        ReadAdminCurationView()
     }
 }
