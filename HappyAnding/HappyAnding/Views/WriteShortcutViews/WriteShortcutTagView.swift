1887f196dfd1ca92090e1aa195d9de84a4eb5adb
JIMIN KIM <jim4020key@gmail.com>
2022-10-25 14:38:11 +0900

    [Feat] #26 - Add horizontal scrollview of tags

--- HappyAnding/HappyAnding/Views/WriteShortcutViews/WriteShortcutTagView.swift
+++ HappyAnding/HappyAnding/Views/WriteShortcutViews/WriteShortcutTagView.swift
@@ -9,7 +9,147 @@ import SwiftUI
 
 struct WriteShortcutTagView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            categoryList()
+            relatedAppList()
+        }
+    }
+    
+    struct categoryList: View {
+        
+        //TODO: 카테고리는 최대 3개 선택 가능
+        
+        @State var categories: [Category] = [Category.lifestyle, Category.education, Category.finance]
+        
+        var body: some View {
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(categories, id:\.self) { item in
+                        categoryCell(item: item.rawValue, items: $categories)
+                    }
+                    Button(action: {
+                        
+                        //TODO: CategoryModalView 열기
+                        
+                    }, label: {
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("카테고리 추가")
+                        }
+                    })
+                    .Body2()
+                    .foregroundColor(.Gray3)
+                    .frame(height: 46)
+                    .padding(.horizontal)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .strokeBorder(Color.Gray3, lineWidth: 1))
+                }
+            }
+            .padding(.leading, 16)
+        }
+    }
+    
+    struct categoryCell: View {
+        var item: String
+        @Binding var items: [Category]
+        
+        var body: some View {
+            HStack {
+                Text(item)
+                
+                Button(action: {
+                    items.removeAll { $0.rawValue == item }
+                }, label: {
+                    Image(systemName: "xmark")
+                })
+            }
+            .Body2()
+            .foregroundColor(.Primary)
+            .frame(height: 46)
+            .padding(.horizontal)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(Color.Primary, lineWidth: 1))
+        }
+    }
+    
+    struct relatedAppList: View {
+        @State var relatedApp = ""
+        @State var relatedApps: [String] = ["지도", "인스타그램"]
+        
+        var body: some View {
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(relatedApps, id:\.self) { item in
+                        relatedAppCell(item: item, items: $relatedApps)
+                    }
+                    
+                    TextField("앱 추가", text: $relatedApp)
+                        .modifier(ClearButton(text: $relatedApp))
+                        .onSubmit {
+                            if !relatedApp.isEmpty {
+                                relatedApps.append(relatedApp)
+                                relatedApp = ""
+                            }
+                        }
+                        .Body2()
+                        .foregroundColor(.Gray3)
+                        .frame(height: 46)
+                        .padding(.horizontal)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .strokeBorder(Color.Gray3, lineWidth: 1))
+                }
+            }
+            .padding(.leading, 16)
+        }
+    }
+    
+    struct relatedAppCell: View {
+        var item: String
+        @Binding var items: [String]
+        
+        var body: some View {
+            HStack {
+                Text(item)
+                
+                Button(action: {
+                    items.removeAll { $0 == item }
+                }, label: {
+                    Image(systemName: "xmark")
+                })
+            }
+            .Body2()
+            .foregroundColor(.Primary)
+            .frame(height: 46)
+            .padding(.horizontal)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(Color.Primary, lineWidth: 1))
+        }
+    }
+    
+    struct ClearButton: ViewModifier {
+        @Binding var text: String
+        
+        public func body(content: Content) -> some View {
+            HStack {
+                if text.isEmpty {
+                    Image(systemName: "plus")
+                }
+                content
+                if !text.isEmpty {
+                    Button(action: {
+                        self.text = ""
+                    }) {
+                        Image(systemName: "x.circle.fill")
+                            .Body2()
+                            .foregroundColor(.Gray4)
+                    }
+                }
+            }
+        }
     }
 }
 
