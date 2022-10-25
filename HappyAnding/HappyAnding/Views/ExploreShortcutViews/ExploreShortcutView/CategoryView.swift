d4638a0515caf6ebb8809203fb499657e8c5cd75
otoolz <otool.hf@gmail.com>
2022-10-25 16:17:04 +0900

    [Feat] 임시 Category enum 삭제
    최선 버젼에서 추가된 Category enum 확인완료
    임시 Category enum 삭제

--- HappyAnding/HappyAnding/Views/ExploreShortcutViews/ExploreShortcutView/CategoryView.swift
+++ HappyAnding/HappyAnding/Views/ExploreShortcutViews/ExploreShortcutView/CategoryView.swift
@@ -7,23 +7,6 @@
 
 import SwiftUI
 
-// TODO: 나중에 지울 예정인 임시 카테고리 enum
-
-enum Category: String, CaseIterable {
-    case education = "교육"
-    case fitness = "건강 및 피트니스"
-    case money = "금융"
-    case life = "라이프스타일"
-    case weather = "날씨"
-    case picture = "사진 및 비디오"
-    case decoration = "데코레이션/꾸미기"
-    case utility = "유틸리티"
-    case social = "소셜 네트워킹"
-    case enterta = "엔터테인먼트"
-    case travel = "여행"
-    case bussness = "비즈니스"
-}
-
 struct CategoryView: View {
     var body: some View {
         VStack {
