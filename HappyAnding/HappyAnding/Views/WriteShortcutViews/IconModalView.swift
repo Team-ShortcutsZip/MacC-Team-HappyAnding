92cf95ead6e90a7fa57a6ae9ddcefdc368563725
JIMIN KIM <jim4020key@gmail.com>
2022-10-25 17:34:53 +0900

    [Feat] #68 - Create WriteShortcutTitleView UI

--- HappyAnding/HappyAnding/Views/WriteShortcutViews/IconModalView.swift
+++ HappyAnding/HappyAnding/Views/WriteShortcutViews/IconModalView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct IconModalView: View {
+    
+    // MARK: WriteShortcutTitleView에서의 확인을 위해 임시로 작성한 코드입니다.
+    
+    @Binding var isShowing: Bool
+    @Binding var iconColor: String
+    @Binding var iconSymbol: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("컬러", text: $iconColor)
+        TextField("심볼", text: $iconSymbol)
+        Button(action: {
+            isShowing = false
+        }, label: {
+            Text("완료")
+        })
+        .disabled(iconColor.isEmpty || iconSymbol.isEmpty)
     }
 }
 
 struct IconModalView_Previews: PreviewProvider {
     static var previews: some View {
-        IconModalView()
+        IconModalView(isShowing: .constant(true), iconColor: .constant("LightPurple"), iconSymbol: .constant("bus.fill"))
     }
 }
--- HappyAnding/HappyAnding/Views/WriteShortcutViews/WriteShortcutTitleView.swift
+++ HappyAnding/HappyAnding/Views/WriteShortcutViews/WriteShortcutTitleView.swift
@@ -8,13 +8,94 @@
 import SwiftUI
 
 struct WriteShortcutTitleView: View {
+    @Binding var shortcutName: String
+    @Binding var shortcutLink: String
+    @Binding var iconColor: String
+    @Binding var iconSymbol: String
+    
+    @State var isShowingIconModal = false
+    @State var isNameValid = false
+    @State var isLinkValid = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                ProgressView(value: 0.33, total: 1)
+                
+                Button(action: {
+                    isShowingIconModal = true
+                }, label: {
+                    if iconSymbol.isEmpty {
+                        ZStack(alignment: .center) {
+                            Rectangle()
+                                .fill(Color.Gray1)
+                                .cornerRadius(12.35)
+                                .frame(width: 84, height: 84)
+                            
+                            Image(systemName: "plus")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.Gray5)
+                        }
+
+                    } else {
+                        ZStack(alignment: .center) {
+                            Rectangle()
+                                .fill(Color.fetchGradient(color: iconColor))
+                                .cornerRadius(12.35)
+                                .frame(width: 84, height: 84)
+                            
+                            Image(systemName: iconSymbol)
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .foregroundColor(.White)
+                        }
+                    }
+                })
+                .padding(36)
+                .sheet(isPresented: $isShowingIconModal) {
+                    IconModalView(isShowing: $isShowingIconModal, iconColor: $iconColor, iconSymbol: $iconSymbol)
+                }
+                
+                ValidationCheckTextField(textType: .mandatory,
+                                         isMultipleLines: false,
+                                         title: "단축어 이름",
+                                         placeholder: "단축어 이름을 입력하세요",
+                                         lengthLimit: 20,
+                                         content: $shortcutName,
+                                         isValid: $isNameValid
+                )
+                .keyboardType(.asciiCapable)
+                
+                ValidationCheckTextField(textType: .mandatory,
+                                         isMultipleLines: false,
+                                         title: "단축어 링크",
+                                         placeholder: "단축어 링크를 추가하세요",
+                                         lengthLimit: 100,
+                                         content: $shortcutLink,
+                                         isValid: $isLinkValid
+                )
+                
+                Spacer()
+                
+                NavigationLink {
+                    WriteShortcutdescriptionView()
+                } label: {
+                    Text("다음")
+                        .Body1()
+                        .frame(maxWidth: .infinity, maxHeight: 52)
+                }
+                .disabled(iconColor.isEmpty || iconSymbol.isEmpty || !isNameValid || !isLinkValid)
+                .padding(.horizontal, 16)
+                .buttonStyle(.borderedProminent)
+            }
+            .ignoresSafeArea(.keyboard)
+        }
     }
 }
 
 struct WriteShortcutTitleView_Previews: PreviewProvider {
     static var previews: some View {
-        WriteShortcutTitleView()
+        WriteShortcutTitleView(shortcutName: .constant(""), shortcutLink: .constant(""), iconColor: .constant("LightPurple"), iconSymbol: .constant("bus.fill"))
     }
 }
