7ca585a4259db532ac70e463a7747e88be6d2f3b
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-10-26 12:06:13 +0900

    [Style] #32 Change WriteCurationView files
    Deleted : SelectShortcutModalView, WriteCurationColorView
    Added : CheckBoxShortcutCell

---
+++ HappyAnding/HappyAnding/Views/WriteCurationViews/CheckBoxShortcutCell.swift
@@ -0,0 +1,20 @@
+//
+//  CheckBoxShortcutCell.swift
+//  HappyAnding
+//
+//  Created by HanGyeongjun on 2022/10/26.
+//
+
+import SwiftUI
+
+struct CheckBoxShortcutCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckBoxShortcutCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckBoxShortcutCell()
+    }
+}
