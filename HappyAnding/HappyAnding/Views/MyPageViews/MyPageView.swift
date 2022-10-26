55c05d7cb88813007ba50f06d80f3af36a4460d8
Elie (Jeon Jimin) <41153398+JMM00@users.noreply.github.com>
2022-10-26 19:35:10 +0900

    [Style] update code convention
    Co-authored-by: Soi (Jiwon Lee) <zest1923@gmail.com>

--- HappyAnding/HappyAnding/Views/MyPageViews/MyPageView.swift
+++ HappyAnding/HappyAnding/Views/MyPageViews/MyPageView.swift
@@ -24,7 +24,9 @@ struct MyPageView: View {
                     //MARK: - 사용자 프로필
                     
                     HStack(spacing: 16) {
+
                         //TODO: 사용되는 임시 이미지 지정되면 변경 필요
+                        
                         Image(systemName: "person.fill")
                             .frame(width: 60, height: 60)
                             .foregroundColor(.White)
