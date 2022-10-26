a9c6e10d9b72c2b772638dcca2f650c999f162b8
JIWON1923 <zest1923@gmail.com>
2022-10-27 01:29:17 +0900

    [Feat] #91 - create userInformation

--- HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadUserCurationView.swift
+++ HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadUserCurationView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct ReadUserCurationView: View {
+    let nickName: String
+    
     var body: some View {
         Text("Hello, World!")
+    }
+    
+    var userInformation: some View {
+        ZStack {
+            
+            HStack {
+                Image(systemName: "person.fill")
+                    .frame(width: 28, height: 28)
+                    .foregroundColor(.White)
+                    .background(Color.Gray3)
+                    .clipShape(Circle())
+                
+                Text(nickName)
+                    .Headline()
+                    .foregroundColor(.Gray4)
+                
+                Spacer()
+                Image(systemName: "light.beacon.max.fill")
+                    .Headline()
+                    .foregroundColor(.Gray5)
+                    .onTapGesture {
+                        
+                        // TODO: 신고기능 연결
+                        
+                        print("Tapped!")
+                    }
+            }
+            .padding(.horizontal, 30)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(height: 48)
+                    .padding(.horizontal, 16)
+                    .foregroundColor(.Gray1)
+            )
+        }
     }
 }
 
