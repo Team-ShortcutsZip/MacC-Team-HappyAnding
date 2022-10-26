116d792df815ee31543470744ed96fa359cf7c40
JIWON1923 <zest1923@gmail.com>
2022-10-27 06:13:19 +0900

    [Feat] #66 - translate Category Name
    Co-authored-by: JMM00 <cjjunn0827@gmail.com>

--- HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutContentView.swift
+++ HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutContentView.swift
@@ -68,13 +68,45 @@ private struct ReusableTextView: View {
                     .foregroundColor(Color.Gray5)
             }
             if let contentsArray {
-                ForEach(contentsArray, id: \.self) { content in
-                    Text(content)
+                ForEach(contentsArray, id: \.self) {
+                    content in
+                    Text(title == "카테고리" ? translateName(content) : content)
                         .Body2()
                         .foregroundColor(Color.Gray5)
                 }
             }
             
+        }
+    }
+    
+    private func translateName(_ categoryName: String) -> String {
+        switch categoryName {
+        case "education":
+            return "교육"
+        case "finance":
+            return "금융"
+        case "business":
+            return "비즈니스"
+        case "health":
+            return "건강"
+        case "lifestyle":
+            return "라이프스타일"
+        case "weather":
+            return "날씨"
+        case "photo":
+            return "사진 및 비디오"
+        case "decoration":
+            return "데코레이션 / 꾸미기"
+        case "utility":
+            return "유틸리티"
+        case "sns":
+            return "소셜 네트워킹"
+        case "entertainment":
+            return "엔터테인먼트"
+        case "trip":
+            return "여행"
+        default:
+            return ""
         }
     }
 }
