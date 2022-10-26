dd9be4a2e3463c923b18c17d4b273488f4749f61
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-10-26 21:28:13 +0900

    [Feat] #32 Create CheckBoxShortcutCell

--- HappyAnding/HappyAnding/Views/WriteCurationViews/CheckBoxShortcutCell.swift
+++ HappyAnding/HappyAnding/Views/WriteCurationViews/CheckBoxShortcutCell.swift
@@ -8,13 +8,89 @@
 import SwiftUI
 
 struct CheckBoxShortcutCell: View {
+    
+    @State private var isTapped: Bool = false
+    
+    let color: String
+    let sfSymbol: String
+    let name: String
+    let description: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color.Background
+            
+            HStack {
+                toggle
+                icon
+                shortcutInfo
+                Spacer()
+            }
+            .padding(.vertical, 20)
+            .background( background )
+            .padding(.horizontal, 16)
+        }
+        .onTapGesture {
+            if isTapped {
+                isTapped = false
+            }
+            else {
+                isTapped = true
+            }
+        }
+        .padding(.top, 12)
+        .background(Color.Background)
     }
-}
+    
+    var toggle: some View {
+        Image(systemName: isTapped ? "checkmark.square.fill" : "square")
+            .foregroundColor(isTapped ? .Primary : .Gray3)
+            .padding(.leading, 20)
+    }
+    
+    var icon: some View {
+        
+        ZStack(alignment: .center) {
+            Rectangle()
+                .fill(Color.fetchGradient(color: color))
+                .cornerRadius(8)
+                .frame(width: 52, height: 52)
+            
+            Image(systemName: sfSymbol)
+                .foregroundColor(.white)
+        }
+        .padding(.leading, 12)
+    }
+    
+    var shortcutInfo: some View {
+        
+        VStack(alignment: .leading, spacing: 4) {
+            Text(name)
+                .Headline()
+                .foregroundColor(.Gray5)
+                .lineLimit(1)
+            Text(description)
+                .Footnote()
+                .foregroundColor(.Gray3)
+                .lineLimit(2)
+        }
+        .padding(.leading, 12)
+        .padding(.trailing, 20)
+    }
+    
+    var background: some View {
+        
+        RoundedRectangle(cornerRadius: 12)
+            .fill(Color.White)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(isTapped ? Color.Primary : Color.Gray1)
+            )
+    }}
 
 struct CheckBoxShortcutCell_Previews: PreviewProvider {
     static var previews: some View {
-        CheckBoxShortcutCell()
+        CheckBoxShortcutCell(color: "Blue", sfSymbol: "books.vertical.fill", name: "ShortcutsTitle", description: "DescriptionDescription")
     }
 }
