84446b936e6eacba55255d1ea826bab4615929d8
Elie (Jeon Jimin) <cjjunn0827@gmail.com>
2022-10-26 19:12:59 +0900

    [Style] #16 - delete icon

--- HappyAnding/HappyAnding/Views/MyPageViews/MyPageView.swift
+++ HappyAnding/HappyAnding/Views/MyPageViews/MyPageView.swift
@@ -33,9 +33,12 @@ struct MyPageView: View {
                                 Text(userName)
                                     .Title1()
                                     .foregroundColor(.Gray5)
+                                //TODO: 스프린트 1에서 배제 추후 주석 삭제 필요
+                                /*
                                 Image(systemName: "square.and.pencil")
                                     .Title2()
                                     .foregroundColor(.Gray4)
+                                 */
                             }
                             Text(userEmail)
                                 .Body2()
