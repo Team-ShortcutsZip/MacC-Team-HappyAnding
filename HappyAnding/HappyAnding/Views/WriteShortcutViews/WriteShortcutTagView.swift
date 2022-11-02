3df05891304bcd063c61de47e6e36fbabb7cb729
JIWON1923 <zest1923@gmail.com>
2022-11-02 14:45:08 +0900

    [Fix] #111 - solve forcequit

--- HappyAnding/HappyAnding/Views/WriteShortcutViews/WriteShortcutTagView.swift
+++ HappyAnding/HappyAnding/Views/WriteShortcutViews/WriteShortcutTagView.swift
@@ -15,7 +15,7 @@ struct WriteShortcutTagView: View {
     
     @State var isShowingCategoryModal = false
     @State var isRequirementValid = false
-    @State var shortcut = Shortcuts(sfSymbol: "", color: "", title: "", subtitle: "", description: "", category: [""], requiredApp: [""], date: "", numberOfLike: 0, numberOfDownload: 0, author: "", shortcutRequirements: "", downloadLink: [""])
+    @State var shortcut = Shortcuts(sfSymbol: "", color: "", title: "", subtitle: "", description: "", category: [String](), requiredApp: [String](), date: "", numberOfLike: 0, numberOfDownload: 0, author: "", shortcutRequirements: "", downloadLink: [""])
     
     let isEdit: Bool
     
