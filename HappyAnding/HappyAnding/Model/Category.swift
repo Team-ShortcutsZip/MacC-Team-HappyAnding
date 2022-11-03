b576c2614fc730bc225ec23685a7e4487652045e
JIWON1923 <zest1923@gmail.com>
2022-11-03 22:10:32 +0900

    [Feat] #157 - udpate category description
    co-authored-by: mxnxxii <m990508m@gmail.com>

--- HappyAnding/HappyAnding/Model/Category.swift
+++ HappyAnding/HappyAnding/Model/Category.swift
@@ -35,29 +35,29 @@ enum Category: String, CaseIterable {
     func fetchDescription() -> String {
         switch self {
         case .education:
-            return "공부, 학업 등 교육 등의 단축어들이 모여있어요"
+            return "공부, 학업, 교육과 관련된 단축어들이 모여있어요"
         case .finance:
-            return "모바일 뱅킹, 주식 등의 단축어들이 모여있어요"
+            return "모바일 뱅킹, 소비와 관련된 단축어들이 모여있어요"
         case .business:
-            return "파일 정리, 파일 확장자 변환 등의 단축어들이 모여있어요"
+            return "파일 관리, 클라우드와 관련된 단축어들이 모여있어요"
         case .health:
-            return "운동, 건강정보 등의 단축어들이 모여있어요"
+            return "운동, 건강정보와 관련된 단축어들이 모여있어요"
         case .lifestyle:
-            return "일기장, 집안 살림 등의 단축어들이 모여있어요"
+            return "일정관리, 취미와 관련된 단축어들이 모여있어요"
         case .weather:
-            return "일기예보, 미세먼지 등의 단축어들이 모여있어요"
+            return "날씨정보와 관련된 단축어들이 모여있어요"
         case .photo:
-            return "이미지 편집기, 사진 촬영 등의 단축어들이 모여있어요"
+            return "이미지 편집, 사진과 관련된 단축어들이 모여있어요"
         case .decoration:
-            return "위젯 꾸미기, 화면 꾸미기 등의 단축어들이 모여있어요"
+            return "위젯 꾸미기, 화면 꾸미기와 관련된 단축어들이 모여있어요"
         case .utility:
-            return "계산기, 압축해제 등의 단축어들이 모여있어요"
+            return "기본설정과 관련된 단축어들이 모여있어요"
         case .sns:
-            return "채팅 어플리케이션 등의 단축어들이 모여있어요"
+            return "커뮤니케이션과 관련된 단축어들이 모여있어요"
         case .entertainment:
-            return "아이돌, 음악 등의 단축어들이 모여있어요"
+            return "방송, 음악과 관련된 단축어들이 모여있어요"
         case .trip:
-            return "번역기, 예매 등의 단축어들이 모여있어요"
+            return "지도, 예매와 관련된 단축어들이 모여있어요"
         }
     }
     
