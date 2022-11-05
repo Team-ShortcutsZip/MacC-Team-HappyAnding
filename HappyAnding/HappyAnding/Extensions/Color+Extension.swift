4941e9d59ee56d13361b8ea772e8c6a782efad47
mxnxxii <m990508m@gmail.com>
2022-11-05 21:57:52 +0900

    [Feat] #122 - Dark Mode Color Setting (Change & Plus)

--- HappyAnding/HappyAnding/Extensions/Color+Extension.swift
+++ HappyAnding/HappyAnding/Extensions/Color+Extension.swift
@@ -16,19 +16,19 @@ extension String {
 
 extension Color {
     ///System Colors
-    static let Background = Color("Background")
-    static let White = Color("White")
+    static let Background = Color("Grey020")
+    static let White = Color("Grey010")
     static let Primary = Color("Primary")
     static let Error = Color("Error")
     static let Danger = Color("Danger")
     static let Success = Color("Success")
     
     ///Text Colors
-    static let Gray1 = Color("Gray1")
-    static let Gray2 = Color("Gray2")
-    static let Gray3 = Color("Gray3")
-    static let Gray4 = Color("Gray4")
-    static let Gray5 = Color("Gray5")
+    static let Gray1 = Color("Grey030")
+    static let Gray2 = Color("Grey040")
+    static let Gray3 = Color("Grey050")
+    static let Gray4 = Color("Grey060")
+    static let Gray5 = Color("Grey070")
     
     //gradient사용하는 곳에서 Color.fetchGradient(color: "Red")와 같이 사용해주세요
     //컬러명은 숫자를 제외하고 UpperCamelCase로 입력해주세요
