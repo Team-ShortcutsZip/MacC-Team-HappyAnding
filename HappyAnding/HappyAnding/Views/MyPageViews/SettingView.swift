e8716f853448a5a202fed851ffdf34a2572406fa
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-11-07 05:27:57 +0900

    [Feat] #210 modify settingView table

--- HappyAnding/HappyAnding/Views/MyPageViews/SettingView.swift
+++ HappyAnding/HappyAnding/Views/MyPageViews/SettingView.swift
@@ -32,27 +32,47 @@ struct SettingView: View {
             
             SettingCell(title: "버전정보", version: "1.0.0")
             
+            //오픈소스 라이선스 버튼
+            Button(action : {
+                //action
+            }) {
+                SettingCell(title: "오픈소스 라이선스")
+            }
+            
+            //개인정보처리방침 버튼
+            Button(action : {
+                //action
+            }) {
+                SettingCell(title: "개인정보처리방침")
+            }
+            
+            //개인정보처리방침 버튼
+            Button(action : {
+                //action
+            }) {
+                SettingCell(title: "개발팀에 관하여")
+            }
+            
+            //개발자에게 연락하기 버튼
             Button(action : {
                 if MFMailComposeViewController.canSendMail() {
                     self.isShowingMailView.toggle()
-                    
                 }
             }) {
                 if MFMailComposeViewController.canSendMail() {
                     SettingCell(title: "개발자에게 연락하기")
-                    
                 }
                 //못 보내는 기기일 때 뜨는 것. 아예 지워도 될 것 같긴 한데 어떻게할까요. 못 보내는 기기의 기준이 확실치 않아서 일단 이렇게 둠.
                 else {
                     SettingCell(title: "문의사항은 shortcutszip@gmail.com 으로 메일 주세요")
                         .multilineTextAlignment(.leading)
                 }
-                
             }
             .sheet(isPresented: $isShowingMailView) {
                 MailView(isShowing: self.$isShowingMailView, result: self.$result)
             }
             
+            //로그아웃 버튼
             Button(action: {
                 self.isTappedSignOutButton = true
             }) {
