2eac81c0ead8b8999268ffdee41ba0992aee81ce
JIWON1923 <zest1923@gmail.com>
2022-11-07 14:29:07 +0900

    [Feat] #203 - create swipe gesture

---
+++ HappyAnding/HappyAnding/Extensions/UINavigationContoller+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UINavigationContoller+Extension.swift
+//  HappyAnding
+//
+//  Created by 이지원 on 2022/11/07.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
+
+extension UINavigationController {
+    // Remove back button text
+    open override func viewWillLayoutSubviews() {
+        navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
+
