791496b5732f46623d2f31921efffd2ecb8634b5
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-11-09 03:56:17 +0900

    [Feat] #232 create withdrawalView UI

--- HappyAnding/HappyAnding/Views/MyPageViews/SettingViews/WithdrawalView.swift
+++ HappyAnding/HappyAnding/Views/MyPageViews/SettingViews/WithdrawalView.swift
@@ -15,14 +15,56 @@ struct WithdrawalView: View {
     @StateObject var userAuth = UserAuth.shared
     @EnvironmentObject var shortcutsZipViewModel: ShortcutsZipViewModel
     
-    @State var isTappedCheckToggle = true
+    @State var isTappedCheckToggle = false
     @State var isTappedSignOutButton = false
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             
+            Text("ShortcutsZip에서 탈퇴 시 다음과 같이 사용자 데이터가 처리됩니다.")
+                .Title2()
+                .foregroundColor(Color.Gray5)
+                .multilineTextAlignment(.leading)
+                .padding(.top, 32)
+                .padding(.bottom, 16)
+            Text("""
+                 탈퇴 시 삭제되는 항목
+                 - 로그인 정보
+                 - 닉네임
+                 - 좋아요한 단축어 목록
+                 - 다운로드한 단축어 목록
+                 
+                 탈퇴 시 삭제되지 않는 항목
+                 - 작성한 단축어
+                 - 작성한 큐레이션
+                 
+                 탈퇴 시 삭제되지 않는 항목들도 재가입 시 프로필에 표시되지 않습니다.
+                 """)
+            .Body1()
+            .foregroundColor(Color.Gray4)
+            .multilineTextAlignment(.leading)
             
             Spacer()
+            
+            HStack(spacing: 8) {
+                Image(systemName: isTappedCheckToggle ? "checkmark.square.fill" : "square")
+                    .Title2()
+                    .foregroundColor(.Gray4)
+                    .onTapGesture {
+                        if isTappedCheckToggle {
+                            isTappedCheckToggle = false
+                        }
+                        else {
+                            isTappedCheckToggle = true
+                        }
+                    }
+                Text("위 내용을 확인했으며 데이터 처리방법에 동의합니다.")
+                    .Body2()
+                    .foregroundColor(.Gray4)
+                    .multilineTextAlignment(.leading)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, 12)
             
             Button(action: {
                 self.isTappedSignOutButton = true
@@ -47,6 +89,7 @@ struct WithdrawalView: View {
             }
         }
         .padding(.horizontal, 16)
+        .background(Color.Background)
         .navigationTitle("탈퇴하기")
     }
     
@@ -66,7 +109,7 @@ struct WithdrawalView: View {
                     }
                 }
             }
-//            try firebaseAuth.signOut( )
+            //            try firebaseAuth.signOut( )
             self.signInStatus = false
         } catch {
             print(error.localizedDescription)
