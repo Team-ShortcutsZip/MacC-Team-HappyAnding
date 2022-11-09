4cd1af2694afe993cdf09ef444e8cf02300a0efa
JIWON1923 <zest1923@gmail.com>
2022-11-09 22:37:25 +0900

    [Feat] #235 - create SearchView UI

---
+++ HappyAnding/HappyAnding/Views/SearchView.swift
@@ -0,0 +1,69 @@
+//
+//  SearchView.swift
+//  HappyAnding
+//
+//  Created by 이지원 on 2022/11/09.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    @State var searchText: String = ""
+    
+    let result = ["result1", "result2", "ressult3"]
+    
+    var body: some View {
+        VStack {
+            searchBar
+            searchResultList
+        }
+    }
+    
+    
+    // MARK: - 검색 바
+    
+    var searchBar: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            TextField("단축어 이름, 앱 이름으로 검색해보세요", text: $searchText)
+                .onSubmit {
+                    
+                    // TODO: Firebase 함수 연결
+                    
+                    print("Firebase 함수 호출")
+                }
+            
+            if !searchText.isEmpty {
+                Image(systemName: "xmark.circle.fill")
+                    .onTapGesture {
+                        withAnimation {
+                            self.searchText = ""
+                        }
+                    }
+            }
+            
+        }
+        .padding(.horizontal, 16)
+    }
+    
+    
+    // MARK: - 검색 결과
+    var searchResultList: some View {
+        List {
+            ForEach(result, id: \.self) { shortcut in
+                
+                Text(shortcut)
+                
+            }
+        }
+    }
+    
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
