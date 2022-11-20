20635375e5e8a7265c007c9ce917d84b76acd38d
JIWON1923 <zest1923@gmail.com>
2022-11-21 08:11:03 +0900

    [Feat] #268 - update comment view

--- HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutCommentView.swift
+++ HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutCommentView.swift
@@ -8,74 +8,94 @@
 import SwiftUI
 
 struct ReadShortcutCommentView: View {
+    @State var comments = [Comment]()
     @State var isReply = true
     
     var body: some View {
-        VStack {
-            comment
-            
-            Spacer()
+        VStack(alignment: .leading) {
+            if comments.isEmpty {
+                Text("등록된 댓글이 없습니다")
+                    .Body2()
+                    .foregroundColor(.Gray4)
+                
+            } else {
+                comment
+                Spacer()
+            }
+        }
+        .onAppear {
+            for _ in 0...10 {
+                let comment = Comment(user_id: "1", date: "2022112211",
+                                      depth: Int.random(in: 0...1),
+                                      contents: "댓글을남겨요")
+                comments.append(comment)
+            }
         }
     }
     
     var comment: some View {
-        
-        HStack(alignment: .top, spacing: 8) {
-            if isReply {
-                Image(systemName: "arrow.turn.down.right")
-                    .foregroundColor(.Gray4)
-            }
-            VStack(alignment: .leading, spacing: 8) {
-                
-                // MARK: 유저 정보
-                
-                HStack(spacing: 8) {
-                    
-                    Circle()
-                        .frame(width: 24, height: 24)
+        ForEach(comments, id: \.self) { comment in
+            
+            HStack(alignment: .top, spacing: 8) {
+                if comment.depth == 0 {
+                    Image(systemName: "arrow.turn.down.right")
                         .foregroundColor(.Gray4)
+                }
+                VStack(alignment: .leading, spacing: 8) {
                     
-                    Text("로미")
+                    // MARK: 유저 정보
+                    
+                    HStack(spacing: 8) {
+                        
+                        Circle()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.Gray4)
+                        
+                        Text(comment.user_id)
+                            .Body2()
+                            .foregroundColor(.Gray4)
+                    }
+                    .padding(.bottom, 4)
+                    
+                    
+                    // MARK: 댓글 내용
+                    
+                    Text(comment.contents)
                         .Body2()
-                        .foregroundColor(.Gray4)
-                }
-                .padding(.bottom, 4)
-                
-                
-                // MARK: 댓글 내용
-                
-                Text("카카오맵 / 네이버지도 선택 가능하도록 추가하였습니다.")
-                    .Body2()
-                    .foregroundColor(.Gray5)
-                
-                
-                // MARK: Button
-                HStack(spacing: 16) {
-                    Button {
-                        print("답글")
-                    } label: {
-                        Text("답글")
-                            .Footnote()
-                            .foregroundColor(.Gray4)
-                    }
+                        .foregroundColor(.Gray5)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     
-                    Button {
-                        print("수정")
-                    } label: {
-                        Text("수정")
-                            .Footnote()
-                            .foregroundColor(.Gray4)
+                    // MARK: Button
+                    HStack(spacing: 16) {
+                        Button {
+                            print("답글")
+                        } label: {
+                            Text("답글")
+                                .Footnote()
+                                .foregroundColor(.Gray4)
+                        }
+                        
+                        Button {
+                            print("수정")
+                        } label: {
+                            Text("수정")
+                                .Footnote()
+                                .foregroundColor(.Gray4)
+                        }
+                        
+                        Button {
+                            print("삭제")
+                        } label: {
+                            Text("삭제")
+                                .Footnote()
+                                .foregroundColor(.Gray4)
+                        }
                     }
-                    
-                    Button {
-                        print("삭제")
-                    } label: {
-                        Text("삭제")
-                            .Footnote()
-                            .foregroundColor(.Gray4)
-                    }
+                    Divider()
+                        .background(Color.Gray1)
                 }
             }
+            .padding(.bottom, 16)
         }
     }
 }
