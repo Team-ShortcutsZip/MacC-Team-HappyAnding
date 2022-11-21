6833d19e7ab7164713898f744482ba157d0d146f
otoolz <otool.hf@gmail.com>
2022-11-21 20:34:41 +0900

    [fix] 딥링크로 전달받은 쿼리가 있을 경우 새 딥링크 안받는 버그 수정
    이미 딥링크로 전달받은 쿼리로 모달이 올라왔을 경우 새 딥링크 안받는 버그 수정

--- HappyAnding/HappyAnding/Views/TabView/ShortcutTabView.swift
+++ HappyAnding/HappyAnding/Views/TabView/ShortcutTabView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
 struct ShortcutTabView: View {
     
     // TODO: StateObject로 선언할 수 있는 다른 로직 구현해보기
+    @Environment(\.scenePhase) private var phase
     @EnvironmentObject var userAuth: UserAuth
     @EnvironmentObject var shorcutsZipViewModel: ShortcutsZipViewModel
     @AppStorage("signInStatus") var signInStatus = false
@@ -46,6 +48,12 @@ struct ShortcutTabView: View {
                 let data = NavigationReadShortcutType(shortcutID: self.tempShortcutId,
                                                       navigationParentView: .myPage)
                 ReadShortcutView(data: data)
+            }
+            .onChange(of: phase) { newPhase in
+                switch newPhase {
+                case .background: isOpenURL = false
+                default: break
+                }
             }
             .onOpenURL { url in
                 if let tab = url.tabIdentifier {
