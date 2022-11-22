7192f281a71f755dac538461a360a8cf54910741
Elie (Jeon Jimin) <cjjunn0827@gmail.com>
2022-11-23 03:39:07 +0900

    [Style] fix indent

--- HappyAnding/HappyAnding/Views/MyPageViews/MyPageView.swift
+++ HappyAnding/HappyAnding/Views/MyPageViews/MyPageView.swift
@@ -16,80 +16,80 @@ struct MyPageView: View {
     }
     
     var body: some View {
-            ScrollView {
-                VStack(spacing: 32) {
+        ScrollView {
+            VStack(spacing: 32) {
+                
+                //MARK: - 사용자 프로필
+                
+                HStack(spacing: 16) {
                     
-                    //MARK: - 사용자 프로필
+                    //TODO: 사용되는 임시 이미지 지정되면 변경 필요
                     
-                    HStack(spacing: 16) {
-
-                        //TODO: 사용되는 임시 이미지 지정되면 변경 필요
-                        
-                        Image(systemName: "person.fill")
-                            .font(.title)
-                            .frame(width: 60, height: 60)
-                            .foregroundColor(.White)
-                            .background(Color.Gray3)
-                            .clipShape(Circle())
-                            HStack {
-                                Text(shortcutsZipViewModel.userInfo?.nickname ?? "User")
-                                    .Title1()
-                                    .foregroundColor(.Gray5)
-                                //TODO: 스프린트 1에서 배제 추후 주석 삭제 필요
-                                /*
-                                Image(systemName: "square.and.pencil")
-                                    .Title2()
-                                    .foregroundColor(.Gray4)
-                                 */
-                            }
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal, 16)
-                    .padding(.top, 35)
-                    
-                    //TODO: - 각 뷰에 해당하는 단축어 목록 전달하도록 변경 필요
-                    
-                    // MARK: - 나의 단축어
-                    
-                    MyShortcutCardListView(shortcuts: shortcutsZipViewModel.shortcutsMadeByUser,
-                                           navigationParentView: .myPage)
-                    
-                    // MARK: - 내가 작성한 큐레이션
-                    
-                    UserCurationListView(data: NavigationListCurationType(type: .myCuration,
-                                                                          title: "내가 작성한 큐레이션",
-                                                                          isAllUser: false,
-                                                                          navigationParentView: .myPage),
-                                         userCurations: $shortcutsZipViewModel.curationsMadeByUser)
-                        .frame(maxWidth: .infinity)
-                    
-                    // MARK: - 좋아요한 단축어
-                    
-                    MyPageShortcutListCell(type: .myLovingShortcut, shortcuts: shortcutsZipViewModel.shortcutsUserLiked)
-                    
-                    // MARK: -다운로드한 단축어
-                    
-                    MyPageShortcutListCell(type: .myDownloadShortcut, shortcuts: shortcutsZipViewModel.shortcutsUserDownloaded)
-                        .padding(.bottom, 44)
-                }
-            }
-            .navigationBarTitle("프로필")
-            .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem {
-                    NavigationLink(value: NavigationSettingView.first) {
-                        Image(systemName: "gearshape.fill")
-                            .Headline()
+                    Image(systemName: "person.fill")
+                        .font(.title)
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(.White)
+                        .background(Color.Gray3)
+                        .clipShape(Circle())
+                    HStack {
+                        Text(shortcutsZipViewModel.userInfo?.nickname ?? "User")
+                            .Title1()
                             .foregroundColor(.Gray5)
+                        //TODO: 스프린트 1에서 배제 추후 주석 삭제 필요
+                        /*
+                         Image(systemName: "square.and.pencil")
+                         .Title2()
+                         .foregroundColor(.Gray4)
+                         */
                     }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 16)
+                .padding(.top, 35)
+                
+                //TODO: - 각 뷰에 해당하는 단축어 목록 전달하도록 변경 필요
+                
+                // MARK: - 나의 단축어
+                
+                MyShortcutCardListView(shortcuts: shortcutsZipViewModel.shortcutsMadeByUser,
+                                       navigationParentView: .myPage)
+                
+                // MARK: - 내가 작성한 큐레이션
+                
+                UserCurationListView(data: NavigationListCurationType(type: .myCuration,
+                                                                      title: "내가 작성한 큐레이션",
+                                                                      isAllUser: false,
+                                                                      navigationParentView: .myPage),
+                                     userCurations: $shortcutsZipViewModel.curationsMadeByUser)
+                .frame(maxWidth: .infinity)
+                
+                // MARK: - 좋아요한 단축어
+                
+                MyPageShortcutListCell(type: .myLovingShortcut, shortcuts: shortcutsZipViewModel.shortcutsUserLiked)
+                
+                // MARK: -다운로드한 단축어
+                
+                MyPageShortcutListCell(type: .myDownloadShortcut, shortcuts: shortcutsZipViewModel.shortcutsUserDownloaded)
+                    .padding(.bottom, 44)
+            }
+        }
+        .navigationBarTitle("프로필")
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            ToolbarItem {
+                NavigationLink(value: NavigationSettingView.first) {
+                    Image(systemName: "gearshape.fill")
+                        .Headline()
+                        .foregroundColor(.Gray5)
                 }
             }
-            .scrollIndicators(.hidden)
-            .background(Color.Background)
-            .navigationDestination(for: NavigationSettingView.self) { _ in
-                SettingView()
-            }
+        }
+        .scrollIndicators(.hidden)
+        .background(Color.Background)
+        .navigationDestination(for: NavigationSettingView.self) { _ in
+            SettingView()
+        }
     }
 }
 
