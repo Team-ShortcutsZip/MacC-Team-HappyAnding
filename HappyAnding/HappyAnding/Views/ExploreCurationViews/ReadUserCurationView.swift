f6151d92e0519581bf01c69f308562e3c9bfc885
otoolz <otool.hf@gmail.com>
2022-11-24 15:24:18 +0900

    [feat] ReadUserCurationView 딥링크를 위한 버튼 추가
    ReadUserCurationView 딥링크를 위한 버튼 추가

--- HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadUserCurationView.swift
+++ HappyAnding/HappyAnding/Views/ExploreCurationViews/ReadUserCurationView.swift
@@ -76,19 +76,10 @@ struct ReadUserCurationView: View {
         .edgesIgnoringSafeArea([.top])
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarItems(trailing: Menu(content: {
-            if self.data.userCuration.author == shortcutsZipViewModel.currentUser() {
-                myCurationMenuSection
-            } else {
-                //다른 사람 큐레이션 볼 때 공유버튼 동작(트레일링 아이템) 제거.
-                // TODO: 2차 스프린트 이후 공유 기능 구현 및 해당 코드 복원
-//                otherCurationMenuSection
-            }
+            curationMenuSection
         }, label: {
-            Image(systemName: self.data.userCuration.author == shortcutsZipViewModel.currentUser() ? "ellipsis" : "square.and.arrow.up")
+            Image(systemName: "ellipsis")
                 .foregroundColor(.Gray4)
-                //다른 사람 큐레이션 볼 때 공유버튼 (트레일링 아이템) opacity 0
-                // TODO: 2차 스프린트 이후 공유 기능 구현 및 해당 코드 제거
-                .opacity(self.data.userCuration.author == shortcutsZipViewModel.currentUser() ? 1 : 0)
         }))
         .fullScreenCover(isPresented: $isWriting) {
             NavigationStack(path: $writeCurationNavigation.navigationPath) {
@@ -112,20 +103,7 @@ struct ReadUserCurationView: View {
                 Text(authorInformation?.nickname ?? "닉네임")
                     .Headline()
                     .foregroundColor(.Gray4)
-                
                 Spacer()
-                //TODO: 스프린트 1에서 배제 , 추후 주석 삭제 필요
-                /*
-                Image(systemName: "light.beacon.max.fill")
-                    .Headline()
-                    .foregroundColor(.Gray5)
-                    .onTapGesture {
-                        
-                        // TODO: 신고기능 연결
-                        
-                        print("Tapped!")
-                    }
-                 */
             }
             .padding(.horizontal, 30)
             .background(
@@ -134,23 +112,6 @@ struct ReadUserCurationView: View {
                     .foregroundColor(.Gray1)
                     .padding(.horizontal, 16)
             )
-            .alert(isPresented: $isTappedDeleteButton) {
-                Alert(title: Text("글 삭제")
-                    .foregroundColor(.Gray5),
-                      message: Text("글을 삭제하시겠습니까?")
-                    .foregroundColor(.Gray5),
-                      primaryButton: .default(Text("닫기"),
-                      action: {
-                    self.isTappedDeleteButton.toggle()
-                }),
-                      secondaryButton: .destructive(
-                        Text("삭제")
-                        , action: {
-                            shortcutsZipViewModel.deleteData(model: self.data.userCuration)
-                            shortcutsZipViewModel.curationsMadeByUser = shortcutsZipViewModel.curationsMadeByUser.filter { $0.id != self.data.userCuration.id }
-                            presentation.wrappedValue.dismiss()
-                }))
-            }
         }
         .onAppear {
             shortcutsZipViewModel.fetchUser(userID: self.data.userCuration.author) { user in
@@ -173,34 +134,7 @@ struct ReadUserCurationView: View {
 
 extension ReadUserCurationView {
     
-    var myCurationMenuSection: some View {
-        
-        Section {
-            Button {
-                self.isWriting.toggle()
-            } label: {
-                Label("편집", systemImage: "square.and.pencil")
-            }
-            
-            // TODO: 함수 구현 필요
-            // TODO: 2차 스프린트 이후 공유 기능 구현 및 해당 코드 복원
-//            Button(action: {
-//                //Place something action here
-//            }) {
-//                Label("공유", systemImage: "square.and.arrow.up")
-//            }
-            Button(role: .destructive, action: {
-                isTappedDeleteButton.toggle()
-                
-                // TODO: firebase delete function
-                
-            }) {
-                Label("삭제", systemImage: "trash.fill")
-            }
-        }
-    }
-    
-    var otherCurationMenuSection: some View {
+    var curationMenuSection: some View {
         Button(action: {
             //Place something action here
         }) {
@@ -209,8 +143,3 @@ extension ReadUserCurationView {
     }
 }
 
-//struct ReadUserCurationView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ReadUserCurationView(userCuration: UserCuration.fetchData(number: 1)[0], nickName: "test")
-//    }
-//}
