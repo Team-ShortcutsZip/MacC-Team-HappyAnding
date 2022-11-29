13b487055d85645693a8433203b5cf1a3ff748c3
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-11-29 15:07:50 +0900

    [Feat] #259 add paste Url func

--- HappyAnding/ShareExtension/CustomShareViewController.swift
+++ HappyAnding/ShareExtension/CustomShareViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import SwiftUI
-//import Social
 import MobileCoreServices
 import UniformTypeIdentifiers
 
@@ -17,57 +16,33 @@ import FirebaseCore
 class CustomShareViewController: UIViewController {
     
     @State var isWriting = false
-//    @State var hostAppShortcutLink: String = "test"
     
-    func urlData() {
+    func pasteUrl() {
         let extensionItems = extensionContext?.inputItems as! [NSExtensionItem]
-              
-              for extensionItem in extensionItems {
-                  if let itemProviders = extensionItem.attachments {
-                      for itemProvider in itemProviders {
-                          // 해당 객체가 있는지 식별
-//                          if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-//                              itemProvider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { result, error in
-////                                  var image: UIImage?
-////                                  if result is UIImage {
-////                                      image = result as? UIImage
-////                                  }
-//
-//                                  if result is URL {
-//                                      let data = try? Data(contentsOf: result as! URL)
-////                                      image = UIImage(data: data!)!
-//                                  }
-//
-////                                  if result is Data {
-////                                      image = UIImage(data: result as! Data)!
-////                                  }
-//                              })
-//                          }
-                          
-                          if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                              itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { result, error in
-                                  let data = NSData.init(contentsOf:result as! URL)
-                                  DispatchQueue.main.async {
-                                      if let urlStr = result {
-//                                          self.hostAppShortcutLink = "\(urlStr)"
-                                          self.setupViews(link: "\(urlStr)")
-                                      }
-                                  }
-                              })
-                          }
-                      }
-                  }
-              }
+        for extensionItem in extensionItems {
+            if let itemProviders = extensionItem.attachments {
+                for itemProvider in itemProviders {
+                    if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+                        itemProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { result, error in
+                            let data = NSData.init(contentsOf:result as! URL)
+                            DispatchQueue.main.async {
+                                if let urlStr = result {
+                                    self.setupViews(link: "\(urlStr)")
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
-        urlData()
+        pasteUrl()
         setupNavBar()
         super.viewDidLoad()
-//                setupViews()
         self.view.backgroundColor = UIColor(Color.Background)
     }
-    
     
     ///Set Navigation Bar
     private func setupNavBar() {
@@ -87,23 +62,10 @@ class CustomShareViewController: UIViewController {
         extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
     
-    
     ///UIHostingView (SwiftUI view)
-//    private lazy var extShortcutsView: UIView = {
-//        let extShortcutsView = UIHostingController(rootView: ShareExtensionWriteShortcutTitleView(isWriting: self.$isWriting, shareExtensionLink: hostAppShortcutLink, isEdit: false))
-//        self.present(extShortcutsView, animated: true)
-//        return UIView()
-//    }()
     private func setupViews(link: String) {
         let extShortcutsView = UIHostingController(rootView: ShareExtensionWriteShortcutTitleView(isWriting: self.$isWriting, shareExtensionLink: link, isEdit: false))
         self.present(extShortcutsView, animated: true)
-//        self.view.addSubview(extShortcutsView)
-//        NSLayoutConstraint.activate([
-//            extShortcutsView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-//            extShortcutsView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-//            extShortcutsView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-//            extShortcutsView.heightAnchor.constraint(equalToConstant: 44)
-//        ])
     }
 }
 
