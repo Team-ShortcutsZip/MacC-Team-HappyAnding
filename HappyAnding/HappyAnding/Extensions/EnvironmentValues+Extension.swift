3448b6356990b678cad8ca42b4501a5eb9942dc4
Elie (Jeon Jimin) <cjjunn0827@gmail.com>
2023-02-07 02:26:09 +0900

    [Style] EnvironmentValues extension과 alerter 파일 구분

--- HappyAnding/HappyAnding/Extensions/EnvironmentValues+Extension.swift
+++ HappyAnding/HappyAnding/Extensions/EnvironmentValues+Extension.swift
@@ -5,20 +5,11 @@
 //  Created by 전지민 on 2023/01/25.
 //
 
-import Foundation
 import SwiftUI
-
-struct LoginAlertKey: EnvironmentKey {
-    static let defaultValue = Alerter()
-}
 
 extension EnvironmentValues {
     var loginAlertKey: Alerter {
         get { return self[LoginAlertKey.self] }
         set { self[LoginAlertKey.self] = newValue }
     }
-}
-
-class Alerter: ObservableObject {
-    @Published var showAlert = false
 }
