a09acf44e0ac759656fc37279cd38f857787eb67
JIMIN KIM <jim4020key@gmail.com>
2023-02-20 17:58:04 +0900

    [Feat] #386 - Change layout

--- HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutContentView.swift
+++ HappyAnding/HappyAnding/Views/ShortcutDetailViews/ReadShortcutView/ReadShortcutContentView.swift
@@ -15,36 +15,52 @@ struct ReadShortcutContentView: View {
     let profileImage: String = "person.crop.circle"
     
     var body: some View {
-            VStack(alignment: .leading, spacing: 24) {
-                ReusableTextView(title: TextLiteral.readShortcutContentViewDescription, contents: shortcut.description, contentsArray: nil)
-                
-                categoryView
-                
-                if !shortcut.requiredApp.isEmpty {
-                    ReusableTextView(title: TextLiteral.readShortcutContentViewRequiredApps, contents: nil, contentsArray: shortcut.requiredApp)
-                }
-                
-                if !shortcut.shortcutRequirements.isEmpty {
-                    ReusableTextView(title: TextLiteral.readShortcutContentViewRequirements, contents: shortcut.shortcutRequirements, contentsArray: nil)
-                }
-                Spacer()
-                    .frame(maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 24) {
+            ReusableTextView(title: TextLiteral.readShortcutContentViewDescription, contents: shortcut.description, contentsArray: nil)
+            
+            SplitList(title: TextLiteral.readShortcutContentViewCategory, content: shortcut.category)
+            
+            if !shortcut.requiredApp.isEmpty {
+                SplitList(title: TextLiteral.readShortcutContentViewRequiredApps, content: shortcut.requiredApp)
             }
-            .padding(.top, 16)
-            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            if !shortcut.shortcutRequirements.isEmpty {
+                ReusableTextView(title: TextLiteral.readShortcutContentViewRequirements, contents: shortcut.shortcutRequirements, contentsArray: nil)
+            }
+            
+            Spacer()
+                .frame(maxHeight: .infinity)
+        }
+        .padding(.top, 16)
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
     
-    var categoryView: some View {
-        VStack(alignment: .leading) {
-            Text(TextLiteral.readShortcutContentViewCategory)
-                .Body2()
-                .foregroundColor(.gray4)
-            
-            HStack(spacing: 8) {
-                ForEach(shortcut.category, id: \.self) { categoryName in
-                    Text(Category(rawValue: categoryName)?.translateName() ?? "")
-                        .Body2()
-                        .foregroundColor(.gray5)
+    private struct SplitList: View {
+        let title: String
+        let content: [String]
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text(title)
+                    .Body2()
+                    .foregroundColor(Color.gray4)
+                
+                HStack(spacing: 8) {
+                    ForEach(content, id: \.self) { item in
+                        if Category.allCases.contains(where: { $0.rawValue == item }) {
+                            Text(Category(rawValue: item)?.translateName() ?? "")
+                                .Body2()
+                                .foregroundColor(Color.gray5)
+                        } else {
+                            Text(item)
+                                .Body2()
+                                .foregroundColor(Color.gray5)
+                        }
+                        if item != content.last {
+                            Text("|")
+                                .foregroundColor(Color.gray2)
+                        }
+                    }
                 }
             }
         }
@@ -92,4 +108,3 @@ private struct ReusableTextView: View {
         .frame(height: self.heigth)
     }
 }
-
