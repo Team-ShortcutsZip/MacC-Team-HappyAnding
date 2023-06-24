bf10a440159816a347491f0aefaea15c74fc0022
jim4020key <jim4020key@gmail.com>
2023-06-25 01:40:47 +0900

    [Chore] #471 - Move ExploreShortcutView to the right folder

--- HappyAnding/HappyAnding/Views/ExploreShortcutViews/ExploreShortcutView.swift
+++ HappyAnding/HappyAnding/Views/ExploreShortcutViews/ExploreShortcutView.swift
@@ -9,27 +9,200 @@ import SwiftUI
 
 struct ExploreShortcutView: View {
     
-    let firebase = FirebaseService()
-    @State var shortcutsArray: [Shortcuts] = []
+    @EnvironmentObject var shortcutsZipViewModel: ShortcutsZipViewModel
+    
+    // TODO: 추후 UpdateInfoView 제작 시 true로 변경해서 cell 보이게 하기
+    @AppStorage("isUpdateAnnnouncementShow") var isUpdateAnnnouncementShow: Bool = false
+    
+    @Binding var isCategoryCellViewFolded: Bool
+    
+    @State var isTappedAnnouncementCell = false
+    @State private var numberOfDisplayedCategories = 6
+    
+    let randomCategories: [Category]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear {
-                firebase.fetchShortcut(model: "Shortcut") { shortcuts in
-                    shortcutsArray = shortcuts
+        ScrollView {
+            VStack(spacing: 32) {
+                if isUpdateAnnnouncementShow {
+                    Button {
+                        isTappedAnnouncementCell = true
+                    } label: {
+                        AnnouncementCell(icon: "updateAppIcon",
+                                         tagName: TextLiteral.updateTag,
+                                         discription: TextLiteral.updateCellDescription,
+                                         isAnnouncementShow: $isUpdateAnnnouncementShow)
+                    }
+                    .id(000)
+                }
+                
+                sectionView(with: .recent)
+                    .id(111)
+                
+                categoryCardView(with: randomCategories[0])
+
+                sectionView(with: .download)
+                
+                categoryCardView(with: randomCategories[1])
+                
+                sectionView(with: .popular)
+                
+                VStack {
+                    HStack {
+                        SubtitleTextView(text: TextLiteral.categoryViewTitle)
+                        
+                        Spacer()
+                                        
+                        Button(action: {
+                            self.isCategoryCellViewFolded.toggle()
+                        }, label: {
+                            MoreCaptionTextView(text: isCategoryCellViewFolded ? TextLiteral.categoryViewUnfold : TextLiteral.categoryViewFold)
+                        })
+                        .onChange(of: isCategoryCellViewFolded) { _ in
+                            numberOfDisplayedCategories = isCategoryCellViewFolded ? 6 : 12
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                        ForEach(Array(Category.allCases.enumerated()), id: \.offset) { index, value in
+                            
+                            let data = NavigationListCategoryShortcutType(shortcuts: [],
+                                                                          categoryName: value,
+                                                                          navigationParentView: .shortcuts)
+                            
+                            if index < numberOfDisplayedCategories {
+                                
+                                categoryCellView(with: value.translateName())
+                                    .navigationLinkRouter(data: data)
+                                
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                    .id(999)
                 }
             }
-            .onTapGesture {
-                firebase.fetchCategoryLikedList(category: "education") { shortcuts in
-                    shortcutsArray = shortcuts
-                    print(shortcutsArray)
+            .padding(.top, 20)
+            .padding(.bottom, 44)
+        }
+        .scrollIndicators(.hidden)
+        .navigationBarTitle(TextLiteral.exploreShortcutViewTitle)
+        .navigationBarTitleDisplayMode(.large)
+        .background(Color.shortcutsZipBackground)
+        .toolbar {
+            ToolbarItem {
+                Image(systemName: "magnifyingglass")
+                    .shortcutsZipHeadline()
+                    .foregroundColor(.gray5)
+                    .navigationLinkRouter(data: NavigationSearch.first)
+            }
+        }
+        .navigationBarBackground ({ Color.shortcutsZipBackground })
+        .sheet(isPresented: $isTappedAnnouncementCell) {
+            UpdateInfoView()
+                .presentationDetents([.large])
+                .presentationDragIndicator(.visible)
+        }
+    }
+    
+}
+
+// MARK: ViewBuilder
+extension ExploreShortcutView {
+    
+    @ViewBuilder
+    private func sectionView(with sectionType: SectionType) -> some View {
+        
+        let shortcuts = sectionType.filterShortcuts(from: shortcutsZipViewModel)
+        
+        VStack(spacing: 0) {
+            HStack {
+                SubtitleTextView(text: sectionType.title)
+                
+                Spacer()
+                
+                MoreCaptionTextView(text: TextLiteral.more)
+                    .navigationLinkRouter(data: NavigationListShortcutType(sectionType: sectionType,
+                                                                           shortcuts: shortcuts,
+                                                                           navigationParentView: .shortcuts))
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 12)
+            
+            ForEach(Array(shortcuts.enumerated()), id:\.offset) { index, shortcut in
+                if index < 3 {
+                    let data = NavigationReadShortcutType(shortcutID:shortcut.id,
+                                                          navigationParentView: .shortcuts)
+                    
+                    ShortcutCell(shortcut: shortcut,
+                                 rankNumber: index + 1,
+                                 navigationParentView: .shortcuts)
+                    .navigationLinkRouter(data: data)
                 }
+            }
+            .background(Color.shortcutsZipBackground)
+        }
+    }
+    
+    @ViewBuilder
+    private func categoryCardView(with category: Category) -> some View {
+        
+        VStack(spacing: 0) {
+            HStack {
+
+                SubtitleTextView(text: category.translateName())
+
+                Spacer()
+
+                MoreCaptionTextView(text: TextLiteral.more)
+                    .navigationLinkRouter(data: NavigationListCategoryShortcutType(shortcuts: [],
+                                                                                   categoryName: category,
+                                                                                   navigationParentView: .shortcuts))
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 12)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(Array((shortcutsZipViewModel.shortcutsInCategory[randomCategories[0].index].enumerated())), id: \.offset) { index, shortcut in
+                        if index < 7 {
+                            let data = NavigationReadShortcutType(
+                                shortcutID: shortcut.id,
+                                navigationParentView: .shortcuts)
+
+                            ShortcutCardCell(
+                                categoryShortcutIcon: shortcut.sfSymbol,
+                                categoryShortcutName: shortcut.title,
+                                categoryShortcutColor: shortcut.color)
+                            .navigationLinkRouter(data: data)
+                        }
+                    }
+                }
+                .padding(.horizontal, 16)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func categoryCellView(with categoryName: String) -> some View {
+        RoundedRectangle(cornerSize: CGSize(width: 12, height: 12))
+            .strokeBorder(Color.gray1, lineWidth: 1)
+            .background(Color.shortcutsZipWhite)
+            .cornerRadius(12)
+            .frame(maxWidth: .infinity, minHeight:48, maxHeight: 48)
+            .overlay {
+                Text(categoryName)
+                    .shortcutsZipBody2()
+                    .foregroundColor(Color.gray5)
             }
     }
 }
 
 struct ExploreShortcutView_Previews: PreviewProvider {
     static var previews: some View {
-        ExploreShortcutView()
+        ExploreShortcutView(isCategoryCellViewFolded: .constant(true), randomCategories: [Category.lifestyle, Category.utility])
     }
 }
+
