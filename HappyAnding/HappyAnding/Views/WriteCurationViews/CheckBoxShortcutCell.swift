aee2cbcf0a626b4a4303aacbe6a71ac41bcd3426
Jeon Jimin <cjjunn0827@gmail.com>
2023-07-08 12:52:23 +0900

    [Feat] #470 - checkboxCell 합치기.
    - viewBuilder 이용
    - idx -> index로 수정

--- HappyAnding/HappyAnding/Views/WriteCurationViews/CheckBoxShortcutCell.swift
+++ HappyAnding/HappyAnding/Views/WriteCurationViews/CheckBoxShortcutCell.swift
@@ -1,85 +1,85 @@
+////
+////  CheckBoxShortcutCell.swift
+////  HappyAnding
+////
+////  Created by HanGyeongjun on 2022/10/26.
+////
 //
-//  CheckBoxShortcutCell.swift
-//  HappyAnding
+//import SwiftUI
 //
-//  Created by HanGyeongjun on 2022/10/26.
+//struct CheckBoxShortcutCell: View {
 //
-
-import SwiftUI
-
-struct CheckBoxShortcutCell: View {
-    
-    @StateObject var viewModel: WriteCurationViewModel
-
-    let idx: Int
-    
-    var body: some View {
-        
-        ZStack {
-            Color.shortcutsZipBackground
-            
-            HStack {
-                toggle
-                icon
-                shortcutInfo
-                Spacer()
-            }
-            .padding(.vertical, 20)
-            .background( background )
-            .padding(.horizontal, 16)
-        }
-        .onTapGesture {
-            viewModel.checkboxCellTapGesture(idx: idx)
-        }
-        .padding(.top, 0)
-        .background(Color.shortcutsZipBackground)
-    }
-    
-    var toggle: some View {
-        Image(systemName: viewModel.isShortcutsTapped[idx] ? "checkmark.square.fill" : "square")
-            .smallIcon()
-            .foregroundColor(viewModel.isShortcutsTapped[idx] ? .shortcutsZipPrimary : .gray3)
-            .padding(.leading, 20)
-    }
-    
-    var icon: some View {
-        
-        ZStack(alignment: .center) {
-            Rectangle()
-                .fill(Color.fetchGradient(color: viewModel.shortcutCells[idx].color))
-                .cornerRadius(8)
-                .frame(width: 52, height: 52)
-            
-            Image(systemName: viewModel.shortcutCells[idx].sfSymbol)
-                .mediumShortcutIcon()
-                .foregroundColor(.white)
-        }
-        .padding(.leading, 12)
-    }
-    
-    var shortcutInfo: some View {
-        
-        VStack(alignment: .leading, spacing: 4) {
-            Text(viewModel.shortcutCells[idx].title)
-                .shortcutsZipHeadline()
-                .foregroundColor(.gray5)
-                .lineLimit(1)
-            Text(viewModel.shortcutCells[idx].subtitle)
-                .shortcutsZipFootnote()
-                .foregroundColor(.gray3)
-                .lineLimit(2)
-        }
-        .padding(.leading, 12)
-        .padding(.trailing, 20)
-    }
-    
-    var background: some View {
-        
-        RoundedRectangle(cornerRadius: 12)
-            .fill(viewModel.isShortcutsTapped[idx] ? Color.shortcutsZipWhite : Color.backgroudList)
-            .overlay(
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(viewModel.isShortcutsTapped[idx] ? Color.shortcutsZipPrimary : Color.backgroudListBorder)
-            )
-    }
-}
+//    @StateObject var viewModel: WriteCurationViewModel
+//
+//    let idx: Int
+//
+//    var body: some View {
+//
+//        ZStack {
+//            Color.shortcutsZipBackground
+//
+//            HStack {
+//                toggle
+//                icon
+//                shortcutInfo
+//                Spacer()
+//            }
+//            .padding(.vertical, 20)
+//            .background( background )
+//            .padding(.horizontal, 16)
+//        }
+//        .onTapGesture {
+//            viewModel.checkboxCellTapGesture(idx: idx)
+//        }
+//        .padding(.top, 0)
+//        .background(Color.shortcutsZipBackground)
+//    }
+//
+//    var toggle: some View {
+//        Image(systemName: viewModel.isShortcutsTapped[idx] ? "checkmark.square.fill" : "square")
+//            .smallIcon()
+//            .foregroundColor(viewModel.isShortcutsTapped[idx] ? .shortcutsZipPrimary : .gray3)
+//            .padding(.leading, 20)
+//    }
+//
+//    var icon: some View {
+//
+//        ZStack(alignment: .center) {
+//            Rectangle()
+//                .fill(Color.fetchGradient(color: viewModel.shortcutCells[idx].color))
+//                .cornerRadius(8)
+//                .frame(width: 52, height: 52)
+//
+//            Image(systemName: viewModel.shortcutCells[idx].sfSymbol)
+//                .mediumShortcutIcon()
+//                .foregroundColor(.white)
+//        }
+//        .padding(.leading, 12)
+//    }
+//
+//    var shortcutInfo: some View {
+//
+//        VStack(alignment: .leading, spacing: 4) {
+//            Text(viewModel.shortcutCells[idx].title)
+//                .shortcutsZipHeadline()
+//                .foregroundColor(.gray5)
+//                .lineLimit(1)
+//            Text(viewModel.shortcutCells[idx].subtitle)
+//                .shortcutsZipFootnote()
+//                .foregroundColor(.gray3)
+//                .lineLimit(2)
+//        }
+//        .padding(.leading, 12)
+//        .padding(.trailing, 20)
+//    }
+//
+//    var background: some View {
+//
+//        RoundedRectangle(cornerRadius: 12)
+//            .fill(viewModel.isShortcutsTapped[idx] ? Color.shortcutsZipWhite : Color.backgroudList)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 12)
+//                    .strokeBorder(viewModel.isShortcutsTapped[idx] ? Color.shortcutsZipPrimary : Color.backgroudListBorder)
+//            )
+//    }
+//}
