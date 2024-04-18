2f9cc542cd768990ae1c5242e4fcadfe6bdb369d
HanGyeongjun <030212hgjun@gmail.com>
2024-04-18 14:50:08 +0900

    [Feat] #526 - Add font files and struct

---
+++ HappyAnding/HappyAnding/Extensions/String/Font+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Font+Extension.swift
+//  HappyAnding
+//
+//  Created by HanGyeongjun on 4/13/24.
+//
+
+import SwiftUI
+
+extension Font {
+    private struct Pretendard {
+        static let regular = "Pretendard-Regular"
+        static let meduim = "Pretendard-Meduim"
+        static let semiBold = "Pretendard-SemiBold"
+        static let bold = "Pretendard-Bold"
+    }
+    
+}
